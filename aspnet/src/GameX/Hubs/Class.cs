c697f8d3246817b9b2abf4aa52c76a63250b0974
Patrik <rambo3swe@gmail.com>
2015-06-22 16:35:14 +0200

    added test hub

---
+++ aspnet/src/GameX/Hubs/Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace Twee.Api.Hubs
+{
+    public class TypedTestHub : Hub<IClient>
+    {
+        private static int _invokeCount = 0;
+
+        public async Task Echo(string message)
+        {
+            await Clients.Caller.Echo(message, Interlocked.Increment(ref _invokeCount));
+        }
+    }
+
+    public interface IClient
+    {
+        Task Echo(string message, int invokeCount);
+        void MethodB(int arg1, int arg2);
+    }
+}
