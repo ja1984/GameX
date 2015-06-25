bb12e48de622ca623edfab264140ca81a380d767
Jonathan Andersson <jonathan@ezy.se>
2015-06-25 08:37:34 +0200

    Added missing file

---
+++ Android/TheKillEveryoneProject/mobile/src/main/java/se/codejunkies/thekilleveryoneproject/Models/Country.java
@@ -0,0 +1,10 @@
+package se.codejunkies.thekilleveryoneproject.Models;
+
+/**
+ * Created by Jack on 2015-06-24.
+ */
+public class Country {
+    public String Name;
+    public float Population;
+    public float CurrentPopulation;
+}
